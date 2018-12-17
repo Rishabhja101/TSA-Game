c2bcf88a1ac658d24aed924ccf54a1ef30cde17f
Rishabh Jain <rishabh101.99@gmail.com>
2018-12-17 10:01:05 -0800

    setup script for main menu

---
+++ TSA Video Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    [SerializeField]
+    private GameObject instructions;
+
+    [SerializeField]
+    private GameObject settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // switches to the next scene to begin the game
+    public void StartGame()
+    {
+        // load scene to start game
+    }
+
+    // shows the GameObject displaying the instructions
+    public void ShowInstructions()
+    {
+        instructions.SetActive(true);
+    }
+
+    // hides the GameObject displaying the instructions
+    public void HideInstructions()
+    {
+        instructions.SetActive(false);
+    }
+
+    // shows the GameObject displaying the settings
+    public void ShowSettings()
+    {
+        settings.SetActive(true);
+    }
+
+    // hides the GameObject displaying the settings
+    public void HideSettings()
+    {
+        settings.SetActive(false);
+    }
+
+    // terminates the application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
